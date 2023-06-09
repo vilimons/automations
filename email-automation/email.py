412cb6afb22fbbfb70b5fef91d9a26d3111ec6b2
brenoyano <brenoyano@gmail.com>
2023-06-09 04:51:27 -0300

    Refactoring with Data Class example

---
+++ email-automation/email.py
@@ -0,0 +1,65 @@
+# import yagmail and its packages
+import yagmail
+import traceback
+from dataclasses import dataclass, field
+from typing import List, NoReturn
+
+
+class SendError(Exception):
+    pass
+
+
+@dataclass
+class SendRecipients:
+    recipients:  List = field(default_factory=list)
+    cc: List = field(default_factory=list)
+    bcc: List = field(default_factory=list)
+
+
+@dataclass
+class EmailContent:
+    subject: List = field(default_factory=list)
+    contents: List = field(default_factory=list)
+    attachments: List = field(default_factory=list)
+
+
+@dataclass
+class Email:
+    yag_connection: yagmail.sender.SMTP
+    recipients_obj: SendRecipients = field(default_factory=list)
+    email_content: EmailContent = field(default_factory=list)
+
+    def send(self) -> NoReturn:
+        try:
+            yag.send(to = self.recipients_obj.recipients, cc = self.recipients_obj.cc, bcc = self.recipients_obj.bcc,
+            subject = self.email_content.subject, contents = self.email_content.contents, 
+            attachments = self.email_content.attachments)
+            print("Email sent successfully!")
+
+        except:
+            print(traceback)
+            raise SendError("Error, email not sent.")
+
+
+if __name__ == "__main__":
+    
+    yag = yagmail.SMTP("limavibrandao@gmail.com", "Your password goes here")
+
+    receipts = SendRecipients(
+    recipients = ["persontosendemail@gmail.com"], 
+    cc = ['persontosendemail+user@gmail.com'], 
+    bcc = ['persontosendemail+user2@gmail.com']
+    )
+
+    content = EmailContent(
+        subject = ["Excel Automated Attachment File"], 
+        contents = ["<h2>Here's the automated excel file you asked for...<h2>"], 
+        attachments = [r'.\automations\excel-automation\pandas.xlsx']
+    )
+
+    send_mail = Email(
+        yag_connection = yag, 
+        recipients_obj = receipts, 
+        email_content = content
+                            ).send()
+
