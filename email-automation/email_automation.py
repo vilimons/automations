3bdda7b67a4fd24d20a255f713f4ba9def3b1f73
Vitor Brandão <48607584+vilimons@users.noreply.github.com>
2023-06-12 14:43:52 -0300

    Refactoring

--- email-automation/email_automation.py
+++ email-automation/email_automation.py
@@ -1,17 +1,64 @@
 # import yagmail and its packages
 import yagmail
+import traceback
+from dataclasses import dataclass, field
+from typing import List, NoReturn, Union
 
-# initiating connection with SMTP Server
-# SMPT = Simples Mail Transfer Protocol, the standard for the transmission of email on a computer network
-yag = yagmail.SMPT("limavibrandao@gmail.com", "Your password goes here")
 
-# wrap the remaining code in a try-except block to handle errors, such as wrong password
+class SendError(Exception):
+    pass
 
-try:
-    yag.send(to = "persontosendemail@gmail.com", cc = 'persontosendemail+user@gmail.com', bcc = 'persontosendemail+user2@gmail.com',
-    subject = "Excel Automated Attachment File", contents = "<h2>Here's the automated excel file you asked for...<h2>", 
-    attachments = r'C:\Users\Administrator\Desktop\projects\automations\excel-automation\pandas.xlsx')
-    print("Email sent successfully!")
 
-except:
-    print("Error, email not sent.")
+@dataclass
+class SendRecipients:
+    recipients:  Union[str,List] = field(default=None)
+    cc: Union[str,List] = field(default=None)
+    bcc: Union[str,List] = field(default=None)
+
+
+@dataclass
+class EmailContent:
+    subject: Union[str,List] = field(default=None)
+    contents: Union[str,List] = field(default=None)
+    attachments: Union[str,List] = field(default=None)
+
+
+@dataclass
+class Email:
+    yag_connection: yagmail.sender.SMTP
+    recipients_obj: SendRecipients = field(default=SendRecipients())
+    email_content: EmailContent = field(default=EmailContent())
+
+    def send(self) -> NoReturn:
+        try:
+            yag.send(to = self.recipients_obj.recipients, cc = self.recipients_obj.cc, bcc = self.recipients_obj.bcc,
+            subject = self.email_content.subject, contents = self.email_content.contents, 
+            attachments = self.email_content.attachments)
+            print("Email sent successfully!")
+
+        except:
+            print(traceback)
+            raise SendError("Error, email not sent.")
+
+
+if __name__ == "__main__":
+    
+    yag = yagmail.SMTP("limavibrandao@gmail.com", "Your password goes here")
+
+    receipts = SendRecipients(
+    recipients = "persontosendemail@gmail.com", 
+    cc = 'persontosendemail+user@gmail.com', 
+    bcc = 'persontosendemail+user2@gmail.com'
+    )
+
+    content = EmailContent(
+        subject = "Excel Automated Attachment File", 
+        contents = "<h2>Here's the automated excel file you asked for...<h2>", 
+        attachments = r'.\automations\excel-automation\pandas.xlsx'
+    )
+
+    send_mail = Email(
+        yag_connection = yag, 
+        recipients_obj = receipts, 
+        email_content = content
+                            ).send()
